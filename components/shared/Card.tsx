47a7bffee8da1bbd13385aca633e4ca2ea319259
Burhan <alpagut1993@gmail.com>
2023-12-26 15:09:35 +0100

    min-w

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -45,10 +45,10 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
       <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
         {!hidePrice && (
           <div className="flex gap-2">
-            <span className="p-semibold-14 min-w-[20px] rounded-full bg-green-100 px-4 py-1 text-green-60">
+            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
               {event.isFree ? "FREE" : `$${event.price}`}
             </span>
-            <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
+            <p className="p-semibold-14 min-w-fit rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
               {event.category.name}
             </p>
           </div>
